1af44de19ac824761b3246c5815621df1a2ff85b
Brandon Muteau <muteaub@gmail.com>
2022-06-30 22:07:18 +0200

    todolist

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div class="about pa-6">
+    <h1>About vuetify todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum reprehenderit iste libero distinctio ex in doloribus tempore deleniti, expedita totam qui a veritatis sequi magnam? Praesentium similique architecto culpa perferendis!
+    Nam laborum dolore tenetur consequatur ipsa commodi tempore harum. Ad molestiae animi ex perspiciatis ipsam eius odio, aut quod neque eos at! Pariatur minima rerum natus aspernatur? Sapiente, quas eaque.
+    Similique alias, vero rem nam pariatur commodi nostrum iure doloribus reiciendis esse et soluta dolores, in vitae ipsam ea id praesentium quisquam illo ratione voluptas, distinctio aliquid explicabo? Commodi, placeat!</p>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum reprehenderit iste libero distinctio ex in doloribus tempore deleniti, expedita totam qui a veritatis sequi magnam? Praesentium similique architecto culpa perferendis!
+    Nam laborum dolore tenetur consequatur ipsa commodi tempore harum. Ad molestiae animi ex perspiciatis ipsam eius odio, aut quod neque eos at! Pariatur minima rerum natus aspernatur? Sapiente, quas eaque.
+    Similique alias, vero rem nam pariatur commodi nostrum iure doloribus reiciendis esse et soluta dolores, in vitae ipsam ea id praesentium quisquam illo ratione voluptas, distinctio aliquid explicabo? Commodi, placeat!</p>    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum reprehenderit iste libero distinctio ex in doloribus tempore deleniti, expedita totam qui a veritatis sequi magnam? Praesentium similique architecto culpa perferendis!
+    Nam laborum dolore tenetur consequatur ipsa commodi tempore harum. Ad molestiae animi ex perspiciatis ipsam eius odio, aut quod neque eos at! Pariatur minima rerum natus aspernatur? Sapiente, quas eaque.
+    Similique alias, vero rem nam pariatur commodi nostrum iure doloribus reiciendis esse et soluta dolores, in vitae ipsam ea id praesentium quisquam illo ratione voluptas, distinctio aliquid explicabo? Commodi, placeat!</p>
+</div>
 </template>
